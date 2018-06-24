245cf086ffff61a759915f1aa0fcc6fa909c7002
williamxww <vivid_xiang@163.com>
2018-06-24 17:08:43 +0800

    update

--- src/main/java/com/bow/kafka/demo/consumer/ConsumerDemo.java
+++ src/main/java/com/bow/kafka/demo/consumer/ConsumerDemo.java
@@ -1,6 +1,7 @@
 package com.bow.kafka.demo.consumer;
 
 
+import com.bow.kafka.demo.producer.ProducerDemo;
 import com.bow.kafka.util.MqConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -11,20 +12,22 @@ import org.slf4j.LoggerFactory;
  * 同一个主题，不同组的两个消费者各自消费不受影响，同组内的消费者竞争消费一条。<br/>
  * Consumer Groups提供了弹性的消费分担模型。当某个Consumer挂掉或者新的Consumer加入，可以re-balance<br/>
  * 自动提交demo(需要设置enable.auto.commit=true，offset自动提交到zk)
+ * @see ProducerDemo
  */
 public class ConsumerDemo {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(ConsumerDemo.class);
 
     public static void main(String[] args) {
         MqConsumer consumer = new MqConsumer();
-        ConsumerRecords<String, String> records = consumer.poll(100);
         consumer.subscribe("mytopic");
-        for (ConsumerRecord<String, String> record : records) {
-            String msg = String.format("partition=%d, offset=%d, key=%s,value=%s", record.partition(), record.offset(),
-                    record.key(), record.value());
-            LOGGER.info(msg);
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(3000);
+            for (ConsumerRecord<String, String> record : records) {
+                String msg = String.format("partition=%d, offset=%d, key=%s,value=%s", record.partition(),
+                        record.offset(), record.key(), record.value());
+                LOGGER.info(msg);
+            }
         }
     }
-
 }
--- src/main/java/com/bow/kafka/demo/producer/ProducerDemo.java
+++ src/main/java/com/bow/kafka/demo/producer/ProducerDemo.java
@@ -1,29 +1,24 @@
 package com.bow.kafka.demo.producer;
 
 import java.io.IOException;
+
+import com.bow.kafka.demo.consumer.ConsumerDemo;
 import com.bow.kafka.util.MqProducer;
 
+/**
+ * producer 的简单示例
+ * @see ConsumerDemo
+ */
 public class ProducerDemo {
 
     public static void main(String[] args) throws IOException {
 
         MqProducer producer1 = new MqProducer();
-        MqProducer producer2 = new MqProducer();
-
-        boolean closeFlag = false;
+        System.out.println("ready.");
         while (true) {
             char close = (char) System.in.read();
-            if (close == 'c') {
-                producer2.close();
-                closeFlag = true;
-                System.out.println("close producer2");
-            }
             System.out.println("send one message");
             producer1.send("mytopic", "2", "value1");
-            if(!closeFlag){
-                producer2.send("mytopic", "2", "value2");
-            }
-
         }
 
     }
